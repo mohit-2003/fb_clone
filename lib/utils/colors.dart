7fd343f46d76e22c97fbc350035db295d63d00cb
Mohit <mohitbro96@gmail.com>
2022-06-04 13:50:16 +0530

    added some models & static data to show on ui

---
+++ lib/utils/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color scaffoldBackgroundColor = Color(0xFFF0F2F5);
+
+const Color fbBlue = Color(0xFF1777F2);
+
+const LinearGradient createRoomGradient = LinearGradient(
+  colors: [Color(0xFF496AE1), Color(0xFFCE48B1)],
+);
+
+const Color online = Color(0xFF4BCB1F);
+
+const LinearGradient storyGradient = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Colors.transparent, Colors.black26],
+);
